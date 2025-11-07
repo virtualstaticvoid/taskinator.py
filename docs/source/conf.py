ff526ed854365b9714ce3c2b2097d41fec235d7d
Chris Stefano <virtualstaticvoid@gmail.com>
2025-11-07 16:28:58 +0000

    customize documentation config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'tasktronaut'
-copyright = '2025, Chris Stefano'
-author = 'Chris Stefano'
+project = "tasktronaut"
+author = "Chris Stefano"
+copyright = f"2025, {author}"  # pylint: disable=redefined-builtin
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",  # Auto-generate docs from docstrings
+    "sphinx_autodoc_typehints",  # Include type hints in docs
+    "sphinx_rtd_theme",  # Use Read the Docs theme
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_title = "tasktronaut Documentation"
